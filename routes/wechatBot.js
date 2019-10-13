77b21fa1add43dbec24df8a3de56e5decd076ae1
zjusharkyu <littleword@163.com>
2019-10-13 21:36:29 +0800

    Update wechatBot.js

--- routes/wechatBot.js
+++ routes/wechatBot.js
@@ -6,7 +6,7 @@ var bookLink = "https://docs.qq.com/sheet/DTXphbHBpc3FFbGdF";
 
     // 值日生标尺
 	baseDay   = new Date( '2019-10-09 00:00:00.000' );
-	baseDuty  = 18-1;   //当日值日的首位同学学号
+	baseDuty  = 22-1;   //当日值日的首位同学学号
 	baseGuard = 22-1;   //没到周二，算上周护校的家长，到周二，算本周的
 
 	// 2019年下半学期
